b4ee003736df819922aa2263e88c0e547d413778
Alejandro Bautista <alex160889@hotmail.com>
2016-11-30 17:11:31 -0600

    Ventana para la clasificacion de procesos.

---
+++ Login/Clasificación de procesos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Clasificación_de_procesos : Form
+    {
+        public Clasificación_de_procesos()
+        {
+            InitializeComponent();
+        }
+    }
+}
