262b92c9bf5efd06ce63355babf1f61297b877fd
asus <asus@DESKTOP-T3KKCAK>
2016-12-01 01:13:36 -0600

    Se Agrega Ventana de Inicio de sesión y se modifica ventana de Programación de cadenas

--- Login/Presentacion/Login.Designer.cs
+++ Login/Presentacion/Login.Designer.cs
@@ -28,42 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.aceptar = new System.Windows.Forms.Button();
             this.salir = new System.Windows.Forms.Button();
+            this.imagen = new System.Windows.Forms.PictureBox();
+            this.usuarioLabel = new System.Windows.Forms.Label();
+            this.UsuarioComboBox = new System.Windows.Forms.ComboBox();
+            this.ContraseñaLabel = new System.Windows.Forms.Label();
+            this.contraseñaTextBox = new System.Windows.Forms.TextBox();
+            this.recordarUsuarioCheckBox = new System.Windows.Forms.CheckBox();
+            this.registrarUsuarioLinkLabel = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.imagen)).BeginInit();
             this.SuspendLayout();
             // 
             // aceptar
             // 
-            this.aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.aceptar.Location = new System.Drawing.Point(278, 197);
+            this.aceptar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.aceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aceptar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.aceptar.Location = new System.Drawing.Point(255, 169);
             this.aceptar.Name = "aceptar";
-            this.aceptar.Size = new System.Drawing.Size(108, 41);
+            this.aceptar.Size = new System.Drawing.Size(111, 37);
             this.aceptar.TabIndex = 0;
             this.aceptar.Text = "Aceptar";
-            this.aceptar.UseVisualStyleBackColor = true;
+            this.aceptar.UseVisualStyleBackColor = false;
             this.aceptar.Click += new System.EventHandler(this.aceptar_Click);
             // 
             // salir
             // 
-            this.salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.salir.Location = new System.Drawing.Point(408, 197);
+            this.salir.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.salir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salir.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.salir.Location = new System.Drawing.Point(387, 169);
             this.salir.Name = "salir";
-            this.salir.Size = new System.Drawing.Size(108, 41);
+            this.salir.Size = new System.Drawing.Size(111, 37);
             this.salir.TabIndex = 1;
             this.salir.Text = "Salir";
-            this.salir.UseVisualStyleBackColor = true;
+            this.salir.UseVisualStyleBackColor = false;
             this.salir.Click += new System.EventHandler(this.salir_Click);
+            // 
+            // imagen
+            // 
+            this.imagen.BackColor = System.Drawing.SystemColors.Control;
+            this.imagen.Image = ((System.Drawing.Image)(resources.GetObject("imagen.Image")));
+            this.imagen.Location = new System.Drawing.Point(24, 12);
+            this.imagen.Name = "imagen";
+            this.imagen.Size = new System.Drawing.Size(148, 161);
+            this.imagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagen.TabIndex = 2;
+            this.imagen.TabStop = false;
+            // 
+            // usuarioLabel
+            // 
+            this.usuarioLabel.AutoSize = true;
+            this.usuarioLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usuarioLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.usuarioLabel.Location = new System.Drawing.Point(190, 46);
+            this.usuarioLabel.Name = "usuarioLabel";
+            this.usuarioLabel.Size = new System.Drawing.Size(67, 18);
+            this.usuarioLabel.TabIndex = 3;
+            this.usuarioLabel.Text = "Usuario";
+            // 
+            // UsuarioComboBox
+            // 
+            this.UsuarioComboBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.UsuarioComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UsuarioComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.UsuarioComboBox.FormattingEnabled = true;
+            this.UsuarioComboBox.Location = new System.Drawing.Point(297, 42);
+            this.UsuarioComboBox.Name = "UsuarioComboBox";
+            this.UsuarioComboBox.Size = new System.Drawing.Size(201, 28);
+            this.UsuarioComboBox.TabIndex = 4;
+            // 
+            // ContraseñaLabel
+            // 
+            this.ContraseñaLabel.AutoSize = true;
+            this.ContraseñaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContraseñaLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ContraseñaLabel.Location = new System.Drawing.Point(189, 82);
+            this.ContraseñaLabel.Name = "ContraseñaLabel";
+            this.ContraseñaLabel.Size = new System.Drawing.Size(102, 20);
+            this.ContraseñaLabel.TabIndex = 5;
+            this.ContraseñaLabel.Text = "Contraseña";
+            // 
+            // contraseñaTextBox
+            // 
+            this.contraseñaTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.contraseñaTextBox.Location = new System.Drawing.Point(297, 79);
+            this.contraseñaTextBox.Multiline = true;
+            this.contraseñaTextBox.Name = "contraseñaTextBox";
+            this.contraseñaTextBox.Size = new System.Drawing.Size(201, 29);
+            this.contraseñaTextBox.TabIndex = 6;
+            // 
+            // recordarUsuarioCheckBox
+            // 
+            this.recordarUsuarioCheckBox.AutoSize = true;
+            this.recordarUsuarioCheckBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.recordarUsuarioCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.recordarUsuarioCheckBox.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.recordarUsuarioCheckBox.Location = new System.Drawing.Point(193, 119);
+            this.recordarUsuarioCheckBox.Name = "recordarUsuarioCheckBox";
+            this.recordarUsuarioCheckBox.Size = new System.Drawing.Size(239, 21);
+            this.recordarUsuarioCheckBox.TabIndex = 7;
+            this.recordarUsuarioCheckBox.Text = "Recordar Nombre de Usuario";
+            this.recordarUsuarioCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // registrarUsuarioLinkLabel
+            // 
+            this.registrarUsuarioLinkLabel.AutoSize = true;
+            this.registrarUsuarioLinkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registrarUsuarioLinkLabel.Location = new System.Drawing.Point(21, 201);
+            this.registrarUsuarioLinkLabel.Name = "registrarUsuarioLinkLabel";
+            this.registrarUsuarioLinkLabel.Size = new System.Drawing.Size(120, 15);
+            this.registrarUsuarioLinkLabel.TabIndex = 8;
+            this.registrarUsuarioLinkLabel.TabStop = true;
+            this.registrarUsuarioLinkLabel.Text = "Registrar Usuario";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 261);
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(539, 236);
+            this.Controls.Add(this.registrarUsuarioLinkLabel);
+            this.Controls.Add(this.contraseñaTextBox);
+            this.Controls.Add(this.ContraseñaLabel);
+            this.Controls.Add(this.UsuarioComboBox);
+            this.Controls.Add(this.usuarioLabel);
+            this.Controls.Add(this.imagen);
             this.Controls.Add(this.salir);
             this.Controls.Add(this.aceptar);
+            this.Controls.Add(this.recordarUsuarioCheckBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Login";
-            this.Text = "Login";
+            this.Text = "Inicio de Sesión";
+            ((System.ComponentModel.ISupportInitialize)(this.imagen)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +176,13 @@
 
         private System.Windows.Forms.Button aceptar;
         private System.Windows.Forms.Button salir;
+        private System.Windows.Forms.PictureBox imagen;
+        private System.Windows.Forms.Label usuarioLabel;
+        private System.Windows.Forms.ComboBox UsuarioComboBox;
+        private System.Windows.Forms.Label ContraseñaLabel;
+        private System.Windows.Forms.TextBox contraseñaTextBox;
+        private System.Windows.Forms.CheckBox recordarUsuarioCheckBox;
+        private System.Windows.Forms.LinkLabel registrarUsuarioLinkLabel;
     }
+
 }
--- Login/Presentacion/VentanaProgramarCadenas.Designer.cs
+++ Login/Presentacion/VentanaProgramarCadenas.Designer.cs
@@ -28,36 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VentanaProgramarCadenas));
             this.salir = new System.Windows.Forms.Button();
+            this.aceptar = new System.Windows.Forms.Button();
+            this.seleccionaCadenaLabel = new System.Windows.Forms.Label();
+            this.seleccionaCadenaTextBox = new System.Windows.Forms.TextBox();
+            this.seleccionaCadenaButton = new System.Windows.Forms.Button();
+            this.PeriodoLabel = new System.Windows.Forms.Label();
+            this.periodoComboBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // salir
             // 
-            this.salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.salir.Location = new System.Drawing.Point(581, 325);
+            this.salir.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.salir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salir.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.salir.Location = new System.Drawing.Point(627, 291);
             this.salir.Name = "salir";
-            this.salir.Size = new System.Drawing.Size(128, 115);
+            this.salir.Size = new System.Drawing.Size(119, 119);
             this.salir.TabIndex = 0;
             this.salir.Text = "Salir";
-            this.salir.UseVisualStyleBackColor = true;
+            this.salir.UseVisualStyleBackColor = false;
             this.salir.Click += new System.EventHandler(this.salir_Click);
+            // 
+            // aceptar
+            // 
+            this.aceptar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.aceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aceptar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.aceptar.Location = new System.Drawing.Point(484, 291);
+            this.aceptar.Name = "aceptar";
+            this.aceptar.Size = new System.Drawing.Size(119, 119);
+            this.aceptar.TabIndex = 1;
+            this.aceptar.Text = "Aceptar";
+            this.aceptar.UseVisualStyleBackColor = false;
+            // 
+            // seleccionaCadenaLabel
+            // 
+            this.seleccionaCadenaLabel.AutoSize = true;
+            this.seleccionaCadenaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seleccionaCadenaLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.seleccionaCadenaLabel.Location = new System.Drawing.Point(54, 33);
+            this.seleccionaCadenaLabel.Name = "seleccionaCadenaLabel";
+            this.seleccionaCadenaLabel.Size = new System.Drawing.Size(153, 18);
+            this.seleccionaCadenaLabel.TabIndex = 2;
+            this.seleccionaCadenaLabel.Text = "Selecciona Cadena";
+            // 
+            // seleccionaCadenaTextBox
+            // 
+            this.seleccionaCadenaTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seleccionaCadenaTextBox.Location = new System.Drawing.Point(217, 27);
+            this.seleccionaCadenaTextBox.Multiline = true;
+            this.seleccionaCadenaTextBox.Name = "seleccionaCadenaTextBox";
+            this.seleccionaCadenaTextBox.Size = new System.Drawing.Size(299, 29);
+            this.seleccionaCadenaTextBox.TabIndex = 3;
+            // 
+            // seleccionaCadenaButton
+            // 
+            this.seleccionaCadenaButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.seleccionaCadenaButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.seleccionaCadenaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seleccionaCadenaButton.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.seleccionaCadenaButton.Location = new System.Drawing.Point(518, 27);
+            this.seleccionaCadenaButton.Name = "seleccionaCadenaButton";
+            this.seleccionaCadenaButton.Size = new System.Drawing.Size(47, 29);
+            this.seleccionaCadenaButton.TabIndex = 4;
+            this.seleccionaCadenaButton.Text = "°°°";
+            this.seleccionaCadenaButton.UseVisualStyleBackColor = false;
+            // 
+            // PeriodoLabel
+            // 
+            this.PeriodoLabel.AutoSize = true;
+            this.PeriodoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.PeriodoLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.PeriodoLabel.Location = new System.Drawing.Point(54, 80);
+            this.PeriodoLabel.Name = "PeriodoLabel";
+            this.PeriodoLabel.Size = new System.Drawing.Size(60, 18);
+            this.PeriodoLabel.TabIndex = 5;
+            this.PeriodoLabel.Text = "Periodo";
+            // 
+            // periodoComboBox
+            // 
+            this.periodoComboBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.periodoComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.periodoComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.periodoComboBox.FormattingEnabled = true;
+            this.periodoComboBox.Items.AddRange(new object[] {
+            "Una Vez",
+            "Diario",
+            "Semanal",
+            "Mensual"});
+            this.periodoComboBox.Location = new System.Drawing.Point(57, 101);
+            this.periodoComboBox.Name = "periodoComboBox";
+            this.periodoComboBox.Size = new System.Drawing.Size(175, 28);
+            this.periodoComboBox.TabIndex = 6;
             // 
             // VentanaProgramarCadenas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(721, 452);
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(773, 432);
+            this.Controls.Add(this.periodoComboBox);
+            this.Controls.Add(this.PeriodoLabel);
+            this.Controls.Add(this.seleccionaCadenaButton);
+            this.Controls.Add(this.seleccionaCadenaTextBox);
+            this.Controls.Add(this.seleccionaCadenaLabel);
+            this.Controls.Add(this.aceptar);
             this.Controls.Add(this.salir);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "VentanaProgramarCadenas";
             this.Text = "Programar Cadenas";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button salir;
+        private System.Windows.Forms.Button aceptar;
+        private System.Windows.Forms.Label seleccionaCadenaLabel;
+        private System.Windows.Forms.TextBox seleccionaCadenaTextBox;
+        private System.Windows.Forms.Button seleccionaCadenaButton;
+        private System.Windows.Forms.Label PeriodoLabel;
+        private System.Windows.Forms.ComboBox periodoComboBox;
     }
 }
