39c7617ae35a820290247436e343d6944a3e0b3c
asus <asus@DESKTOP-T3KKCAK>
2016-12-02 21:40:40 -0600

    Isaac: Ventana de Programacion de Cadenas Terminada, Falta funcionalidad.

--- Login/Presentacion/VentanaProgramarCadenas.Designer.cs
+++ Login/Presentacion/VentanaProgramarCadenas.Designer.cs
@@ -36,6 +36,27 @@
             this.seleccionaCadenaButton = new System.Windows.Forms.Button();
             this.PeriodoLabel = new System.Windows.Forms.Label();
             this.periodoComboBox = new System.Windows.Forms.ComboBox();
+            this.DiasSemanaGroupBox = new System.Windows.Forms.GroupBox();
+            this.domingoCheckBox = new System.Windows.Forms.CheckBox();
+            this.sabadoCheckBox = new System.Windows.Forms.CheckBox();
+            this.ViernesCheckBox = new System.Windows.Forms.CheckBox();
+            this.JuevesCheckBox = new System.Windows.Forms.CheckBox();
+            this.miercolesCheckBox = new System.Windows.Forms.CheckBox();
+            this.MartesCheckBox = new System.Windows.Forms.CheckBox();
+            this.lunesCheckBox = new System.Windows.Forms.CheckBox();
+            this.FechaHoraGroupBox = new System.Windows.Forms.GroupBox();
+            this.FechaLabel = new System.Windows.Forms.Label();
+            this.FechaDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.mesLabel = new System.Windows.Forms.Label();
+            this.mesComboBox = new System.Windows.Forms.ComboBox();
+            this.horaLabel = new System.Windows.Forms.Label();
+            this.horaTextBox = new System.Windows.Forms.TextBox();
+            this.diasMesLabel = new System.Windows.Forms.Label();
+            this.diasMesTextBox = new System.Windows.Forms.TextBox();
+            this.LogoFintechPictureBox = new System.Windows.Forms.PictureBox();
+            this.DiasSemanaGroupBox.SuspendLayout();
+            this.FechaHoraGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoFintechPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // salir
@@ -44,9 +65,9 @@
             this.salir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.salir.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.salir.Location = new System.Drawing.Point(627, 291);
+            this.salir.Location = new System.Drawing.Point(657, 307);
             this.salir.Name = "salir";
-            this.salir.Size = new System.Drawing.Size(119, 119);
+            this.salir.Size = new System.Drawing.Size(104, 104);
             this.salir.TabIndex = 0;
             this.salir.Text = "Salir";
             this.salir.UseVisualStyleBackColor = false;
@@ -58,9 +79,9 @@
             this.aceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.aceptar.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.aceptar.Location = new System.Drawing.Point(484, 291);
+            this.aceptar.Location = new System.Drawing.Point(537, 307);
             this.aceptar.Name = "aceptar";
-            this.aceptar.Size = new System.Drawing.Size(119, 119);
+            this.aceptar.Size = new System.Drawing.Size(104, 104);
             this.aceptar.TabIndex = 1;
             this.aceptar.Text = "Aceptar";
             this.aceptar.UseVisualStyleBackColor = false;
@@ -101,11 +122,11 @@
             // PeriodoLabel
             // 
             this.PeriodoLabel.AutoSize = true;
-            this.PeriodoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.PeriodoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PeriodoLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.PeriodoLabel.Location = new System.Drawing.Point(54, 80);
             this.PeriodoLabel.Name = "PeriodoLabel";
-            this.PeriodoLabel.Size = new System.Drawing.Size(60, 18);
+            this.PeriodoLabel.Size = new System.Drawing.Size(67, 18);
             this.PeriodoLabel.TabIndex = 5;
             this.PeriodoLabel.Text = "Periodo";
             // 
@@ -125,12 +146,216 @@
             this.periodoComboBox.Size = new System.Drawing.Size(175, 28);
             this.periodoComboBox.TabIndex = 6;
             // 
+            // DiasSemanaGroupBox
+            // 
+            this.DiasSemanaGroupBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.DiasSemanaGroupBox.Controls.Add(this.domingoCheckBox);
+            this.DiasSemanaGroupBox.Controls.Add(this.sabadoCheckBox);
+            this.DiasSemanaGroupBox.Controls.Add(this.ViernesCheckBox);
+            this.DiasSemanaGroupBox.Controls.Add(this.JuevesCheckBox);
+            this.DiasSemanaGroupBox.Controls.Add(this.miercolesCheckBox);
+            this.DiasSemanaGroupBox.Controls.Add(this.MartesCheckBox);
+            this.DiasSemanaGroupBox.Controls.Add(this.lunesCheckBox);
+            this.DiasSemanaGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.DiasSemanaGroupBox.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.DiasSemanaGroupBox.Location = new System.Drawing.Point(57, 146);
+            this.DiasSemanaGroupBox.Name = "DiasSemanaGroupBox";
+            this.DiasSemanaGroupBox.Size = new System.Drawing.Size(459, 112);
+            this.DiasSemanaGroupBox.TabIndex = 7;
+            this.DiasSemanaGroupBox.TabStop = false;
+            this.DiasSemanaGroupBox.Text = "Dias de la Semana";
+            // 
+            // domingoCheckBox
+            // 
+            this.domingoCheckBox.AutoSize = true;
+            this.domingoCheckBox.Location = new System.Drawing.Point(89, 65);
+            this.domingoCheckBox.Name = "domingoCheckBox";
+            this.domingoCheckBox.Size = new System.Drawing.Size(83, 21);
+            this.domingoCheckBox.TabIndex = 6;
+            this.domingoCheckBox.Text = "Domingo";
+            this.domingoCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // sabadoCheckBox
+            // 
+            this.sabadoCheckBox.AutoSize = true;
+            this.sabadoCheckBox.Location = new System.Drawing.Point(6, 65);
+            this.sabadoCheckBox.Name = "sabadoCheckBox";
+            this.sabadoCheckBox.Size = new System.Drawing.Size(76, 21);
+            this.sabadoCheckBox.TabIndex = 5;
+            this.sabadoCheckBox.Text = "Sabado";
+            this.sabadoCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ViernesCheckBox
+            // 
+            this.ViernesCheckBox.AutoSize = true;
+            this.ViernesCheckBox.Location = new System.Drawing.Point(335, 28);
+            this.ViernesCheckBox.Name = "ViernesCheckBox";
+            this.ViernesCheckBox.Size = new System.Drawing.Size(75, 21);
+            this.ViernesCheckBox.TabIndex = 4;
+            this.ViernesCheckBox.Text = "Viernes";
+            this.ViernesCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // JuevesCheckBox
+            // 
+            this.JuevesCheckBox.AutoSize = true;
+            this.JuevesCheckBox.Location = new System.Drawing.Point(257, 28);
+            this.JuevesCheckBox.Name = "JuevesCheckBox";
+            this.JuevesCheckBox.Size = new System.Drawing.Size(72, 21);
+            this.JuevesCheckBox.TabIndex = 3;
+            this.JuevesCheckBox.Text = "Jueves";
+            this.JuevesCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // miercolesCheckBox
+            // 
+            this.miercolesCheckBox.AutoSize = true;
+            this.miercolesCheckBox.Location = new System.Drawing.Point(164, 28);
+            this.miercolesCheckBox.Name = "miercolesCheckBox";
+            this.miercolesCheckBox.Size = new System.Drawing.Size(87, 21);
+            this.miercolesCheckBox.TabIndex = 2;
+            this.miercolesCheckBox.Text = "Miercoles";
+            this.miercolesCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // MartesCheckBox
+            // 
+            this.MartesCheckBox.AutoSize = true;
+            this.MartesCheckBox.Location = new System.Drawing.Point(89, 28);
+            this.MartesCheckBox.Name = "MartesCheckBox";
+            this.MartesCheckBox.Size = new System.Drawing.Size(70, 21);
+            this.MartesCheckBox.TabIndex = 1;
+            this.MartesCheckBox.Text = "Martes";
+            this.MartesCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // lunesCheckBox
+            // 
+            this.lunesCheckBox.AutoSize = true;
+            this.lunesCheckBox.Location = new System.Drawing.Point(6, 28);
+            this.lunesCheckBox.Name = "lunesCheckBox";
+            this.lunesCheckBox.Size = new System.Drawing.Size(66, 21);
+            this.lunesCheckBox.TabIndex = 0;
+            this.lunesCheckBox.Text = "Lunes";
+            this.lunesCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // FechaHoraGroupBox
+            // 
+            this.FechaHoraGroupBox.Controls.Add(this.diasMesTextBox);
+            this.FechaHoraGroupBox.Controls.Add(this.diasMesLabel);
+            this.FechaHoraGroupBox.Controls.Add(this.horaTextBox);
+            this.FechaHoraGroupBox.Controls.Add(this.horaLabel);
+            this.FechaHoraGroupBox.Controls.Add(this.mesComboBox);
+            this.FechaHoraGroupBox.Controls.Add(this.mesLabel);
+            this.FechaHoraGroupBox.Controls.Add(this.FechaDateTimePicker);
+            this.FechaHoraGroupBox.Controls.Add(this.FechaLabel);
+            this.FechaHoraGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.FechaHoraGroupBox.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.FechaHoraGroupBox.Location = new System.Drawing.Point(57, 264);
+            this.FechaHoraGroupBox.Name = "FechaHoraGroupBox";
+            this.FechaHoraGroupBox.Size = new System.Drawing.Size(459, 147);
+            this.FechaHoraGroupBox.TabIndex = 8;
+            this.FechaHoraGroupBox.TabStop = false;
+            this.FechaHoraGroupBox.Text = "Fecha/Hora";
+            // 
+            // FechaLabel
+            // 
+            this.FechaLabel.AutoSize = true;
+            this.FechaLabel.Location = new System.Drawing.Point(10, 23);
+            this.FechaLabel.Name = "FechaLabel";
+            this.FechaLabel.Size = new System.Drawing.Size(47, 17);
+            this.FechaLabel.TabIndex = 0;
+            this.FechaLabel.Text = "Fecha";
+            // 
+            // FechaDateTimePicker
+            // 
+            this.FechaDateTimePicker.Location = new System.Drawing.Point(13, 43);
+            this.FechaDateTimePicker.Name = "FechaDateTimePicker";
+            this.FechaDateTimePicker.Size = new System.Drawing.Size(200, 23);
+            this.FechaDateTimePicker.TabIndex = 1;
+            // 
+            // mesLabel
+            // 
+            this.mesLabel.AutoSize = true;
+            this.mesLabel.Location = new System.Drawing.Point(10, 83);
+            this.mesLabel.Name = "mesLabel";
+            this.mesLabel.Size = new System.Drawing.Size(34, 17);
+            this.mesLabel.TabIndex = 2;
+            this.mesLabel.Text = "Mes";
+            // 
+            // mesComboBox
+            // 
+            this.mesComboBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.mesComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mesComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.mesComboBox.FormattingEnabled = true;
+            this.mesComboBox.Items.AddRange(new object[] {
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Mayo",
+            "Junio",
+            "Julio",
+            "Agosto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre"});
+            this.mesComboBox.Location = new System.Drawing.Point(13, 103);
+            this.mesComboBox.Name = "mesComboBox";
+            this.mesComboBox.Size = new System.Drawing.Size(175, 28);
+            this.mesComboBox.TabIndex = 3;
+            // 
+            // horaLabel
+            // 
+            this.horaLabel.AutoSize = true;
+            this.horaLabel.Location = new System.Drawing.Point(241, 23);
+            this.horaLabel.Name = "horaLabel";
+            this.horaLabel.Size = new System.Drawing.Size(39, 17);
+            this.horaLabel.TabIndex = 4;
+            this.horaLabel.Text = "Hora";
+            // 
+            // horaTextBox
+            // 
+            this.horaTextBox.Location = new System.Drawing.Point(244, 43);
+            this.horaTextBox.Multiline = true;
+            this.horaTextBox.Name = "horaTextBox";
+            this.horaTextBox.Size = new System.Drawing.Size(175, 28);
+            this.horaTextBox.TabIndex = 5;
+            // 
+            // diasMesLabel
+            // 
+            this.diasMesLabel.AutoSize = true;
+            this.diasMesLabel.Location = new System.Drawing.Point(241, 83);
+            this.diasMesLabel.Name = "diasMesLabel";
+            this.diasMesLabel.Size = new System.Drawing.Size(89, 17);
+            this.diasMesLabel.TabIndex = 6;
+            this.diasMesLabel.Text = "Dias del Mes";
+            // 
+            // diasMesTextBox
+            // 
+            this.diasMesTextBox.Location = new System.Drawing.Point(244, 103);
+            this.diasMesTextBox.Multiline = true;
+            this.diasMesTextBox.Name = "diasMesTextBox";
+            this.diasMesTextBox.Size = new System.Drawing.Size(175, 27);
+            this.diasMesTextBox.TabIndex = 7;
+            // 
+            // LogoFintechPictureBox
+            // 
+            this.LogoFintechPictureBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.LogoFintechPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("LogoFintechPictureBox.Image")));
+            this.LogoFintechPictureBox.Location = new System.Drawing.Point(537, 101);
+            this.LogoFintechPictureBox.Name = "LogoFintechPictureBox";
+            this.LogoFintechPictureBox.Size = new System.Drawing.Size(199, 144);
+            this.LogoFintechPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.LogoFintechPictureBox.TabIndex = 9;
+            this.LogoFintechPictureBox.TabStop = false;
+            // 
             // VentanaProgramarCadenas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(773, 432);
+            this.Controls.Add(this.LogoFintechPictureBox);
+            this.Controls.Add(this.FechaHoraGroupBox);
+            this.Controls.Add(this.DiasSemanaGroupBox);
             this.Controls.Add(this.periodoComboBox);
             this.Controls.Add(this.PeriodoLabel);
             this.Controls.Add(this.seleccionaCadenaButton);
@@ -142,6 +367,11 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "VentanaProgramarCadenas";
             this.Text = "Programar Cadenas";
+            this.DiasSemanaGroupBox.ResumeLayout(false);
+            this.DiasSemanaGroupBox.PerformLayout();
+            this.FechaHoraGroupBox.ResumeLayout(false);
+            this.FechaHoraGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoFintechPictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,5 +386,23 @@
         private System.Windows.Forms.Button seleccionaCadenaButton;
         private System.Windows.Forms.Label PeriodoLabel;
         private System.Windows.Forms.ComboBox periodoComboBox;
+        private System.Windows.Forms.GroupBox DiasSemanaGroupBox;
+        private System.Windows.Forms.CheckBox domingoCheckBox;
+        private System.Windows.Forms.CheckBox sabadoCheckBox;
+        private System.Windows.Forms.CheckBox ViernesCheckBox;
+        private System.Windows.Forms.CheckBox JuevesCheckBox;
+        private System.Windows.Forms.CheckBox miercolesCheckBox;
+        private System.Windows.Forms.CheckBox MartesCheckBox;
+        private System.Windows.Forms.CheckBox lunesCheckBox;
+        private System.Windows.Forms.GroupBox FechaHoraGroupBox;
+        private System.Windows.Forms.DateTimePicker FechaDateTimePicker;
+        private System.Windows.Forms.Label FechaLabel;
+        private System.Windows.Forms.ComboBox mesComboBox;
+        private System.Windows.Forms.Label mesLabel;
+        private System.Windows.Forms.TextBox diasMesTextBox;
+        private System.Windows.Forms.Label diasMesLabel;
+        private System.Windows.Forms.TextBox horaTextBox;
+        private System.Windows.Forms.Label horaLabel;
+        private System.Windows.Forms.PictureBox LogoFintechPictureBox;
     }
 }
